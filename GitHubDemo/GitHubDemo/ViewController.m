64828ff4f7dfcf3d1e9a21597933ee878c5a2a19
GavinQuasim <2581812806@qq.com>
2017-04-06 17:02:52 +0800

    测试Xcode更新代码到git

--- GitHubDemo/GitHubDemo/ViewController.m
+++ GitHubDemo/GitHubDemo/ViewController.m
@@ -16,13 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    self.view.backgroundColor = [UIColor orangeColor];
+    //测试git提交
 }
 
 
